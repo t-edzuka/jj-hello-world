009e00a7a3f4935e91898aac2028ad1a7a2dca31
Tomoya Edzuka <t.edzuka@happy-quality.jp>
2024-12-09 13:17:19 +0900

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello())
+    println!("{}", goodbye())
+}
+
+fn hello() -> String {
+    return "Hello, world!".into()
+}
+
+fn goodbye() -> String {
+    return "Goodbye, world!".into()
 }
