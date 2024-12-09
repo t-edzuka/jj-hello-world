4e9c39e156ec30ddaac69c8b574ea36203760e59
Tomoya Edzuka <t.edzuka@happy-quality.jp>
2024-12-09 15:25:00 +0900

    Add comment to main() fn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /// A "Hello, world!" program.
 ///
 /// This is the best implementation of this program to ever exist.
-
+/// The main function runs when our program starts
 fn main() {
     print(hello());
 }
